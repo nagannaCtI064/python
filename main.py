37774a81bd867417b0a8c44bedb69459befd75b2
nagannaCtI064 <119935869+nagannaCtI064@users.noreply.github.com>
2022-12-06 17:31:12 +0530

    pythons

---
+++ main.py
@@ -0,0 +1,26 @@
+import math
+
+num1=input("enter any number:")
+num1=int(num1)
+
+if num1%2 == 0 or num1%4 ==0 or num1%6 == 0 or num1%8==0:
+    print("The number is devisible by 2")
+elif num1%3==0:
+    print("the number devisible by 3")
+elif num1%4==0:
+    print("the number  devisible by 4")
+elif num1%5==0:
+    print("the number devisible by 5")
+elif num1%6==0:
+    print("the number devisible by 6")
+elif num1%7==0:
+    print("the number is devible by 7")
+elif num1%8==0:
+    print("the number is devisible by 8")
+elif num1%9==0:
+    print("the number is devisibele by 9")
+elif num1%10==0:
+    print("the number is devisble by  10")
+import numpy
+a[6]=input("enter a number:")
+
